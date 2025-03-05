c9c4d261d17b63747634338fe91ab1f0fce4c5cb
sdrabdalfattah <sdrabdalfattah@gmail.com>
2025-03-05 17:09:34 +0100

    Add files via upload

---
+++ models/todo.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  userId: String,
+  task: String,
+  completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = Todo;
